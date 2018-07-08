2b025730e104637c1860ca40c3aef0a83d06157b
Pablo Argueta <pabloargueta27@gmail.com>
2018-07-08 23:26:17 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,6 +14,16 @@ def first_challenge
   }
 
   #your code here
+  
+  contacts.each { |person, data|
+    data.each { |key, value|
+      if key == :favorite_icecream_flavors
+        value.delete_if { |flavor|
+          flavor == "strawberry"
+        }
+      end
+    }
+    }
 
 
   #remember to return your newly altered contacts hash!
